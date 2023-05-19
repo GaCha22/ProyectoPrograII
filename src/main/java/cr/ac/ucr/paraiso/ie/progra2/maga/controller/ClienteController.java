78e7556d7de769d061350ee577e54b09934ed5f8
GaCha22 <gchavesrojas@gmail.com>
2023-05-19 17:58:29 -0600

    Prototipo de interfaces hecho

--- src/main/java/cr/ac/ucr/paraiso/ie/progra2/maga/controller/ClienteController.java
+++ src/main/java/cr/ac/ucr/paraiso/ie/progra2/maga/controller/ClienteController.java
@@ -1,20 +1,31 @@
 package cr.ac.ucr.paraiso.ie.progra2.maga.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableListBase;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class ClienteController {
+
+
     @FXML
-    private Label welcomeText;
+    private ChoiceBox<String> chbTipo;
+    @FXML
+    private TextArea txtaDatos;
 
     @FXML
     void initialize(){
-
+        ObservableList<String> items = FXCollections.observableArrayList("Avioneta", "Avión comercial", "Avión de carga");
+        chbTipo.setItems(items);
+        chbTipo.setValue("Tipo de avión");
     }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    void onActionGenerarReporte(ActionEvent event) {
     }
 
 }
