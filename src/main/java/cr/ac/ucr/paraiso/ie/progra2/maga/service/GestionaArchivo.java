c2a287fd540d2ffa99fcdb3fa7bdda8d9175d7d5
varela13dani <116319948+varela13dani@users.noreply.github.com>
2023-06-01 01:05:05 -1100

    model & archivoConfiguracion

--- src/main/java/cr/ac/ucr/paraiso/ie/progra2/maga/service/GestionaArchivo.java
+++ src/main/java/cr/ac/ucr/paraiso/ie/progra2/maga/service/GestionaArchivo.java
@@ -1,36 +1,54 @@
 package cr.ac.ucr.paraiso.ie.progra2.maga.service;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import cr.ac.ucr.paraiso.ie.progra2.maga.model.Pista;
-import cr.ac.ucr.paraiso.ie.progra2.maga.model.Puerta;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
+
+//https://github.com/fangyidong/json-simple , usar esta libreria?
 
 public class GestionaArchivo {
 
-    public void leerArchivo() {
+        public void leerArchivo()
+        {
+            JSONParser parser = new JSONParser();
+            FileReader fileReader;
+            try {
+                File file = new File("config.json");
+                fileReader = new FileReader(file);
 
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        try {
-            // Leer el archivo JSON y convertirlo en un objeto raíz
-            File file = new File("config.json");
-
-            JsonNode rootNode = objectMapper.readTree(file);
-
-            // Obtener el arreglo de pistas
-            JsonNode pistasNode = rootNode.get("pistas");
-            List<Pista> pistas = objectMapper.convertValue(pistasNode, new TypeReference<List<Pista>>() {});
-
-            JsonNode puertasNode = rootNode.get("puertas");
-            List<Puerta> puertas = objectMapper.convertValue(puertasNode, new TypeReference<List<Puerta>>() {});
+                JSONArray array = (JSONArray) parser.parse(fileReader);
 
 
-        } catch (IOException e) {
-            e.printStackTrace();
+                for(int i=0;i<array.size();i++)
+                {
+                    JSONObject obj =  (JSONObject)array.get(i);
+                    parseObject(obj);
+                }
+
+            }
+
+            catch(FileNotFoundException e)
+            {
+                System.out.println(e.getStackTrace()+ " :File Not Found");
+            }
+            catch(ParseException e)
+            {
+                System.out.println(e.getStackTrace()+ " :Could not parse");
+            }
+            catch(IOException e)
+            {
+                System.out.println(e.getStackTrace()+ " :IOException");
+            }
         }
+
+    private void parseObject(JSONObject obj) {
+        JSONArray pistas= (JSONArray) obj.get("pistas");
+        JSONArray puertas = (JSONArray) obj.get("puertas");
     }
 
 }
