41c80bc7e55978f51d0e9975c96f6f2dfb6b839f
GaCha22 <gchavesrojas@gmail.com>
2023-06-01 23:34:55 -0600

    Cambios varios

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
-module cr.ac.ucr.paraiso.ie.progra2.gama {
+module cr.ac.ucr.paraiso.ie.progra2.maga {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.google.gson;
 
+    exports cr.ac.ucr.paraiso.ie.progra2.maga.service;
     exports cr.ac.ucr.paraiso.ie.progra2.maga.controller;
     opens cr.ac.ucr.paraiso.ie.progra2.maga.controller to javafx.fxml;
     exports cr.ac.ucr.paraiso.ie.progra2.maga;
