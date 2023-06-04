ff54fb9ef92b0bcb5305ad63f491b0d7950d6f39
Aarón <aaronchacon2601@gmail.com>
2023-06-04 15:15:31 -0600

    Botones funcionales, commit anterior

--- src/main/java/cr/ac/ucr/paraiso/ie/progra2/maga/controller/ClienteController.java
+++ src/main/java/cr/ac/ucr/paraiso/ie/progra2/maga/controller/ClienteController.java
@@ -6,6 +6,7 @@ import javafx.collections.ObservableList;
 import javafx.collections.ObservableListBase;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
@@ -17,6 +18,12 @@ public class ClienteController {
     private ChoiceBox<String> chbTipo;
     @FXML
     private TextArea txtaDatos;
+    @FXML
+    private Button btnDespegar;
+    @FXML
+    private Button btnAterrizar;
+    @FXML
+    private Button btnIrAPuerta;
     private Piloto piloto;
 
     @FXML
@@ -31,7 +38,20 @@ public class ClienteController {
     }
 
     @FXML
-    void onActionGenerarReporte(ActionEvent event) {
+    void onActionIrAPuerta(ActionEvent a) {
+        btnAterrizar.setDisable(true);
+        btnDespegar.setDisable(false);
     }
 
+    @FXML
+    void onActionAterrizar(ActionEvent a) {
+        btnDespegar.setDisable(true);
+        btnIrAPuerta.setDisable(false);
+    }
+
+    @FXML
+    void onActionDespegar(ActionEvent a) {
+        btnIrAPuerta.setDisable(true);
+        btnAterrizar.setDisable(false);
+    }
 }
--- src/main/java/cr/ac/ucr/paraiso/ie/progra2/maga/controller/ServidorController.java
+++ src/main/java/cr/ac/ucr/paraiso/ie/progra2/maga/controller/ServidorController.java
@@ -1,6 +1,7 @@
 package cr.ac.ucr.paraiso.ie.progra2.maga.controller;
 
 import cr.ac.ucr.paraiso.ie.progra2.maga.servidor.MultiServidor;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class ServidorController {
@@ -11,5 +12,14 @@ public class ServidorController {
     void initialize(){
         multiServidor = new MultiServidor();
         multiServidor.start();
+    }
+
+    public void onActionPonerEnEspera(ActionEvent a) {
+    }
+
+    public void onActionAceptarSolicitud(ActionEvent a) {
+    }
+
+    public void onActionGenerarReporte(ActionEvent a) {
     }
 }
