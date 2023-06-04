dab78447bb5a76461492e048adcaf5a3bde09c5e
Aarón <aaronchacon2601@gmail.com>
2023-06-04 17:19:52 -0600

    Commit anterior del borrado

--- src/main/java/cr/ac/ucr/paraiso/ie/progra2/maga/controller/ClienteController.java
+++ src/main/java/cr/ac/ucr/paraiso/ie/progra2/maga/controller/ClienteController.java
@@ -6,17 +6,22 @@ import javafx.collections.ObservableList;
 import javafx.collections.ObservableListBase;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
 public class ClienteController {
-
-
     @FXML
     private ChoiceBox<String> chbTipo;
     @FXML
     private TextArea txtaDatos;
+    @FXML
+    private Button btnDespegar;
+    @FXML
+    private Button btnAterrizar;
+    @FXML
+    private Button btnIrAPuerta;
     private Piloto piloto;
 
     @FXML
@@ -31,7 +36,20 @@ public class ClienteController {
     }
 
     @FXML
-    void onActionGenerarReporte(ActionEvent event) {
+    void onActionIrAPuerta(ActionEvent a) {
+        btnAterrizar.setDisable(true);
+        btnDespegar.setDisable(false);
     }
 
+    @FXML
+    void onActionAterrizar(ActionEvent a) {
+        btnDespegar.setDisable(true);
+        btnIrAPuerta.setDisable(false);
+    }
+
+    @FXML
+    void onActionDespegar(ActionEvent a) {
+        btnIrAPuerta.setDisable(true);
+        btnAterrizar.setDisable(false);
+    }
 }
