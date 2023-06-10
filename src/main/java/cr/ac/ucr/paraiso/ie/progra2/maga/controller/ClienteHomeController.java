af9394ae0ea9fc61b550144d0e1f8b050c618ad5
luisballar <luisballar123@gmail.com>
2023-06-09 14:49:35 -1100

    Placas casi hecho

--- src/main/java/cr/ac/ucr/paraiso/ie/progra2/maga/controller/ClienteHomeController.java
+++ src/main/java/cr/ac/ucr/paraiso/ie/progra2/maga/controller/ClienteHomeController.java
@@ -3,6 +3,7 @@ package cr.ac.ucr.paraiso.ie.progra2.maga.controller;
 import cr.ac.ucr.paraiso.ie.progra2.maga.ClienteMain;
 import cr.ac.ucr.paraiso.ie.progra2.maga.model.Aeronave;
 import cr.ac.ucr.paraiso.ie.progra2.maga.model.CompaniaAerea;
+import cr.ac.ucr.paraiso.ie.progra2.maga.model.Vuelo;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -12,6 +13,8 @@ import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClienteHomeController {
     @FXML
@@ -54,17 +57,42 @@ public class ClienteHomeController {
         }
     }
 
+    private boolean utilityAeronaves(String serialInsertada){
+        boolean encontrado = false;
+        for (int i=0; i<aviones.size(); i++) {
+            if(aeronave.getPlaca().equals(serialInsertada)){
+                encontrado = true;
+            }
+        }
+        return encontrado;
+    }
+
+    List<Aeronave> aviones = new ArrayList<>();
+
     @FXML
     void onActionGuardar(ActionEvent a) {
+
+
         if (!chbAerolinea.getValue().equals("Aerolíneas") && !chbTipo.getValue().equals("Tipo de avión") && !txtPlaca.getText().equals("")) {
             companiaAerea = new CompaniaAerea(chbAerolinea.getValue());
             int tipo = chbTipo.getValue().equals("Avioneta") ? 3 : chbTipo.getValue().equals("Avión comercial") ? 1 : 2;
             aeronave = new Aeronave(txtPlaca.getText(), tipo);
-            loadPage("interfaz/cliente.fxml");
-            clienteController.setVuelo(aeronave, companiaAerea);
-            clienteController.setTextTXT("Tipo: " + aeronave +
-                    "\nPlaca: " + aeronave.getPlaca() +
-                    "\nAerolínea: " + companiaAerea.getNombre() + "\n");
+            aviones.add(aeronave);
+
+            if(!utilityAeronaves(txtPlaca.getText())){
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("No pueden haber dos seriales iguales");
+                alert.setHeaderText(null);
+                alert.setContentText("Cambie el serial");
+                alert.showAndWait();
+
+            }else {
+                loadPage("interfaz/cliente.fxml");
+                clienteController.setVuelo(aeronave, companiaAerea);
+                clienteController.setTextTXT("Tipo: " + aeronave +
+                        "\nPlaca: " + aeronave.getPlaca() +
+                        "\nAerolínea: " + companiaAerea.getNombre() + "\n");
+            }
         }else{
             Alert alert = new Alert(Alert.AlertType.WARNING);
             alert.setTitle("Error al guardar");
