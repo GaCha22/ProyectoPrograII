3ae2c10ec408d77acf5963cfe0e67976f2a05411
Aarón Chacón <aaronchacon2601@gmail.com>
2023-06-14 08:41:08 -0600

    Terminado

--- src/main/java/cr/ac/ucr/paraiso/ie/progra/maga/controller/ServidorController.java
+++ src/main/java/cr/ac/ucr/paraiso/ie/progra/maga/controller/ServidorController.java
@@ -34,28 +34,33 @@ public class ServidorController implements PropertyChangeListener{
     public TextArea txtaPuertasDisponibles;
     private MultiServidor multiServidor;
     public static Stage stage;
+
     @FXML
     void initialize() throws IOException {
+        //Se le da los valores al multiservidor que posee el config.json
         multiServidor = new MultiServidor(GestionaArchivo.leerArchivoConfiguracion("config.json"));
         multiServidor.agregarPropertyChangeListener(this);
-        if (!multiServidor.isAlive()) multiServidor.start();
+        if (!multiServidor.isAlive()) multiServidor.start(); //Si el servidor no está en ejecución inicializa
         File archivo = new File("placasRegistradas.txt");
+        //Se elimina el archivo placasRegistradas.txt si existe
         if (archivo.exists()) {
             archivo.delete();
         }
-        archivo.createNewFile(); //de esta manera el archivo se limpia cuando el servidor se carga
+        archivo.createNewFile(); //De esta manera el archivo se limpia cuando el servidor se carga
         txtaPistasDisponibles.setText(MultiServidor.aeropuertoServer.pistasToString());
         txtaPuertasDisponibles.setText(MultiServidor.aeropuertoServer.puertasToString());
         setTable();
         setSolicitudSeleccionada();
     }
 
+    //Se le da la funcionalidad al botón Aceptar Solicitud
     @FXML
     void onActionAceptarSolicitud(ActionEvent actionEvent) {
+        //Si hay una solicitud en cola se acepta
         if(MultiServidor.getClientsInQueue().peek() != null) {
             MultiServidor.getClientsInQueue().peek().aceptarSolicitud();
             setSolicitudSeleccionada();
-        }else {
+        }else { //Si no hay solicitudes se alerta al usuario
             Alert alert = new Alert(Alert.AlertType.WARNING);
             alert.setHeaderText(null);
             alert.setContentText("No haz seleccionado una petición");
@@ -63,11 +68,13 @@ public class ServidorController implements PropertyChangeListener{
         }
     }
 
+    //Se le da la funcionalidad al botón En Espera
     @FXML
     void onActionEnEsperar(ActionEvent actionEvent) {
+        //Si hay una solicitud se pone en espera
         if(MultiServidor.getClientsInQueue().peek() != null)
             MultiServidor.getClientsInQueue().peek().ponerEnEspera();
-        else {
+        else { //Si no hay solicitudes se alerta al usuario
             Alert alert = new Alert(Alert.AlertType.WARNING);
             alert.setHeaderText(null);
             alert.setContentText("No haz seleccionado una petición");
@@ -75,6 +82,7 @@ public class ServidorController implements PropertyChangeListener{
         }
     }
 
+    //Método que carga la "escena" desde el documento FXML
     private void loadPage(String page) {
         FXMLLoader fxmlLoader = new FXMLLoader(ClienteMain.class.getResource(page));
         try {
@@ -87,11 +95,14 @@ public class ServidorController implements PropertyChangeListener{
         }
     }
 
+    //Se le da la funcionalidad al botón Generar Reporte
+    //Se muestran los registros que posee reporte.json
     @FXML
     void onActionGenerarReporte(ActionEvent actionEvent) {
         loadPage("interfaz/reporte.fxml");
     }
 
+    //Método para dar los valores al TableView
     private void setTable(){
         TableColumn<List<String>, String> column = new TableColumn<>("Placa");
         column.setCellValueFactory(data->new SimpleStringProperty(data.getValue().get(0)));
@@ -105,12 +116,14 @@ public class ServidorController implements PropertyChangeListener{
         TableColumn<List<String>, String> column4 = new TableColumn<>("Solicitud");
         column4.setCellValueFactory(data->new SimpleStringProperty(data.getValue().get(3)));
 
+        //Se agregan todas las columnas al TableView
         tblSolcitudes.getColumns().add(column);
         tblSolcitudes.getColumns().add(column2);
         tblSolcitudes.getColumns().add(column3);
         tblSolcitudes.getColumns().add(column4);
     }
 
+    //Método en el cual se obtiene la información de la aeronave para enviarla al multiservidor
     public ObservableList<List<String>> getData() {
         ObservableList<List<String>> data = FXCollections.observableArrayList();
         int count = 0;
@@ -131,17 +144,22 @@ public class ServidorController implements PropertyChangeListener{
         return data;
     }
 
+    //Método en el cual se le da valor al TextArea de la Solicitud Seleccionada
     private void setSolicitudSeleccionada(){
         tblSolcitudes.setItems(getData());
+        //Si existe una solicitud la escribe en el TextArea
         if (MultiServidor.peekSolicitudInQueue()!=null) txtaSolicitudSeleccionada.setText(MultiServidor.peekSolicitudInQueue().getVuelo().getAeronave().getPlaca());
-        else txtaSolicitudSeleccionada.setText("No hay solicitudes");
+        else txtaSolicitudSeleccionada.setText("No hay solicitudes"); //Si no hay solicitudes lo indica
     }
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         String solicitud = (String) evt.getNewValue();
+        //Si la solicitud es igual a "actualizar"
         if (solicitud.equals("actualizar")){
+            //Se coloca la nueva solicitud
             setSolicitudSeleccionada();
+            //Se muestran las pistas y puertas disponibles
             txtaPistasDisponibles.setText(MultiServidor.aeropuertoServer.pistasToString());
             txtaPuertasDisponibles.setText(MultiServidor.aeropuertoServer.puertasToString());
         }else if (solicitud.equals("consume")){}
